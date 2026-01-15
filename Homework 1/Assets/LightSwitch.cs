3daed10eeaad9cd235544659be54a74c41f834bc
bitteina <banskihanski@gmail.com>
2026-01-15 19:20:00 +0200

    Light switch works

--- Homework 1/Assets/LightSwitch.cs
+++ Homework 1/Assets/LightSwitch.cs
@@ -5,14 +5,20 @@ using UnityEngine.InputSystem;
 public class LightSwitch : MonoBehaviour
 {
     public Light light;
+    public InputActionReference action;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         light = GetComponent<Light>();
+        action.action.Enable();
         //Here should check if B button is pressed!! tab only temporarily
-        if (Input.GetKeyDown("tab"))
+        //if (Input.GetKeyDown("tab"))
+        //{
+        //    light.color = Color.blue;
+        //}
+        action.action.performed += (ctx) =>
         {
             light.color = Color.blue;
-        }
+        };
     }
 }
