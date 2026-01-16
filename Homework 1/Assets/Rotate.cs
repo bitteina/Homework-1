d0f7e1ce1576935036e4f92f9539d6603c0c65d6
bitteina <banskihanski@gmail.com>
2026-01-16 17:38:20 +0200

    Faster moon

--- Homework 1/Assets/Rotate.cs
+++ Homework 1/Assets/Rotate.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class Rotate : MonoBehaviour
 {
-    public float degreesPerSecond = 2.0f;
+    public float degreesPerSecond = 5.0f;
     void Update()
     {
         transform.Rotate(0, degreesPerSecond * Time.deltaTime, 0);
