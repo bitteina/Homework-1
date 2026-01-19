ef56e317e9a740a93a583474bca4e4d6399cdca4
bitteina <banskihanski@gmail.com>
2026-01-19 11:36:11 +0200

    Updated teleport, new build

--- Homework 1/Assets/Teleport.cs
+++ Homework 1/Assets/Teleport.cs
@@ -1,19 +1,28 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+//Teleports player to a platform outside the room
 public class Teleport : MonoBehaviour
 {
+    //To check if player has teleported to the platform outside
+    public bool teleported = false;
+    //Action here is 'trigger' on right hand controller
     public InputActionReference action;
     void Start()
     {
         action.action.Enable();
         action.action.performed += (ctx) =>
         {
-            if (transform.position == Vector3.zero)
+            //Checking if teleported to the platform or not
+            if (teleported == false)
             {
+                //Not teleported --> Teleport 30 units to the right (platform location)
                 transform.position = Vector3.right * 30f;
+                teleported = true;
             } else {
+                //Has teleported --> Teleport back to starting position
                 transform.position = Vector3.zero;
+                teleported = false;
             }
         };
     }
